3a574f9d74dc0e0a22380d221ebb241848cc1ae5
maximecastelli <maximecastelli@yahoo.fr>
2021-10-03 15:43:52 +0200

    Init project - setup Next + Sass

---
+++ pages/holding.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Holding() {
+  return (
+    <ul>
+      <li>
+        <h5>Item 1</h5>
+      </li>
+      <li>
+        <h5>Item 2</h5>
+      </li>
+      <li>
+        <h5>Item 3</h5>
+      </li>
+      <li>
+        <h5>Item 4</h5>
+      </li>
+      <li>
+        <h5>Item 5</h5>
+      </li>
+    </ul>
+  );
+}
